ca50df24a4bd4ef2710f36cf72c8801764cf767f
Dinesh <dineshpabbi10@gmail.com>
2019-05-27 16:25:11 +0530

    make it better

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
     $(window).scroll( function(){
         $('.fadein').each( function(i){
             
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight()/4;
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight()/10;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             if( bottom_of_window > bottom_of_element ){
